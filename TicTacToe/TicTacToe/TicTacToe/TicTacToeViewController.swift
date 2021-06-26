9f7c09cb24ea389c5e54125dc5458a84f290a8ae
Erdi Kanik <erdikanik@gmail.com>
2021-06-27 00:18:28 +0300

    Add view model state change handler to view controller

--- TicTacToe/TicTacToe/TicTacToe/TicTacToeViewController.swift
+++ TicTacToe/TicTacToe/TicTacToe/TicTacToeViewController.swift
@@ -11,4 +11,33 @@ final class TicTacToeViewController: UIViewController {
 
     @IBOutlet private weak var collectionView: UICollectionView!
     @IBOutlet private weak var stateLabel: UILabel!
+
+    private var viewModel: TicTacToeViewModelInterface = TicTacToeViewModel()
+    private var stateNames: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewModel.stateChangeHandler = { [weak self] state in
+            switch state {
+            case .gameStarted(let types, let stateName):
+                self?.updateStateAndCells(types: types, stateName: stateName)
+            case .gameStateChanged(let types, let stateName):
+                self?.updateStateAndCells(types: types, stateName: stateName)
+            case.gameFinished(_):
+                break
+            }
+        }
+    }
+}
+
+// MARK - Private helper methods
+
+private extension TicTacToeViewController {
+
+    func updateStateAndCells(types: [String], stateName: String) {
+        stateNames = types
+        stateLabel.text = stateName
+        collectionView.reloadData()
+    }
 }
